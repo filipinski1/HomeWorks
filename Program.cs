db554e87637ecf465b11557713373eb6a7b45d57
Filipinski <140195559+filipinski1@users.noreply.github.com>
2023-12-12 14:35:55 -0500

    added file to GitIgnore

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using Google.Cloud.Translation.V2;
 
-namespace HomeWorks
+namespace TranslationApiDemo
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
+
+
         {
-            Console.WriteLine("Hello World!");
+            // Set the path to your JSON key file
+            string keyFilePath = @"C:\Users\mliak\Documents\code\translate\homework10-407901-51543db07a37.json";
+
+            // Set the GOOGLE_APPLICATION_CREDENTIALS environment variable
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", keyFilePath);
+
+            // Create a TranslationClient using the credentials
+            var client = TranslationClient.Create();
+
+            // Your translation logic goes here
+            var text = "Hello, how are you?";
+            var response = client.TranslateText(text, LanguageCodes.Turkish);
+
+            Console.WriteLine($"Original: {text}");
+            Console.WriteLine($"Translated: {response.TranslatedText}");
         }
     }
 }
