ed891ccaef33034f91c8f437e5a354ed6c8d8126
Filipinski <140195559+filipinski1@users.noreply.github.com>
2023-12-13 10:16:29 -0500

    added rest of the languages for user to pick

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Google.Cloud.Translation.V2;
 
 namespace TranslationApiDemo
@@ -18,12 +19,52 @@ namespace TranslationApiDemo
             // Create a TranslationClient using the credentials
             var client = TranslationClient.Create();
 
-            // Your translation logic goes here
-            var text = "Hello, how are you?";
-            var response = client.TranslateText(text, LanguageCodes.Turkish);
+            var supportedLanguages = client.ListLanguages();
 
-            Console.WriteLine($"Original: {text}");
-            Console.WriteLine($"Translated: {response.TranslatedText}");
+            // Display the list of supported languages to the user
+            Console.WriteLine("Supported Languages:");
+
+            // Create a dictionary to store language code and name mappings
+            var languageDictionary = new Dictionary<string, string>();
+
+            foreach (var language in supportedLanguages)
+            {
+                Console.WriteLine($"{language.Code}: {language.Name}");
+                languageDictionary[language.Code] = language.Name;
+            }
+
+            // Get user input for the text to be translated
+            Console.Write("Enter the text to be translated: ");
+            string text = Console.ReadLine();
+
+            // Get user input for the target language
+            Console.Write("Enter the target language code (e.g., 'es' for Spanish): ");
+            string targetLanguageCode = Console.ReadLine();
+
+            if (languageDictionary.TryGetValue(targetLanguageCode, out var targetLanguageName))
+            {
+                Console.WriteLine($"Selected language: {targetLanguageName}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid language code. Using default.");
+                targetLanguageCode = "en"; // Use English as the default language
+            }
+
+            try
+            {
+                // Your translation logic goes here
+                var response = client.TranslateText(text, targetLanguageCode);
+                Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+                Console.WriteLine($"Original: {text}");
+                Console.WriteLine($"Translated: {response.TranslatedText}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
         }
     }
-}
+    }
+
