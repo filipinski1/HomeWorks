b3faf9f66bf6b44f2b9b8ef5be3b7eaf1d66e20f
Filipinski <140195559+filipinski1@users.noreply.github.com>
2024-01-22 20:21:32 -0500

    commit

---
+++ Translator.cs
@@ -0,0 +1,41 @@
+﻿using Google.Cloud.Translation.V2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorks
+{
+    internal class Translator : ITranslator
+    {
+        private readonly TranslationClient _client;
+
+        public Translator(TranslationClient client)
+        {
+            _client = client;
+        }
+        public string DetectLanguage(string text)
+        {
+            var detection = _client.DetectLanguage(text);
+            return detection.Language;
+        }
+
+        public List<string> GetTranslationLanguages()
+        {
+            var languages = new List<string>();
+            foreach (var language in _client.ListLanguages(LanguageCodes.English))
+            {
+                languages.Add(language.Name);
+
+            }
+            return languages;
+        }
+
+        public string Translate(string text, string targetLanguage)
+        {
+            var response = _client.TranslateText(text, targetLanguage);
+            return response.TranslatedText;
+        }
+    }
+}
